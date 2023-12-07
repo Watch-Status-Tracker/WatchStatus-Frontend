0e376d9a6c527b248b79a0eead81ce9ad53bee6b
Numer <numerpriv@gmail.com>
2023-12-07 01:40:05 +0100

    add: jsconfig with absolute paths

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import jsconfigPaths from 'vite-jsconfig-paths';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [react(), eslint(), jsconfigPaths()],
 });
