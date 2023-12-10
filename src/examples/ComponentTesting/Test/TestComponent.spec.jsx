cb5b5315f807f6b47e6ead9f62fb515496671ea2
Numer <numerpriv@gmail.com>
2023-12-11 00:44:08 +0100

    add: example component with prop types, styles and unit tests

---
+++ src/examples/ComponentTesting/Test/TestComponent.spec.jsx
@@ -0,0 +1,28 @@
+import { render } from '@config/tests';
+import { TestComponent } from '@examples/ComponentTesting/Test/Test';
+import '@testing-library/jest-dom';
+
+// Grouping tests
+describe('Example for TestComponent', () => {
+  // Single test case
+  it('should render the text', () => {
+    /* 
+    Render the component and get the instance of the 
+    component using getByText method 
+    */
+    const { getByText } = render(<TestComponent text="Hello World" />);
+    /* Check if the text is rendered properly */
+    expect(getByText('Hello World')).toBeInTheDocument();
+  });
+
+  it('should render the component with proper background color', () => {
+    const { getByText } = render(<TestComponent text="Hello World" />);
+    /* 
+      Check if the component has the proper background color because
+      it is styled using styled-components theme provider
+    */
+    expect(getByText('Hello World')).toHaveStyle(`
+      background: rgb(255,0,0);
+    `);
+  });
+});
---
+++ src/examples/ComponentTesting/Test/TestComponent.styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+// Styles for component using styled-components
+export const Wrapper = styled.div`
+  // Using theme values from theme provider
+  background: ${({ theme }) => theme.color.red};
+`;
