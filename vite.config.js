97e11ba98b13b5c26b5d979f86b0080fb57f1ac2
Numer <numerpriv@gmail.com>
2023-12-11 00:36:37 +0100

    add: vitest configuration

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import jsconfigPaths from 'vite-jsconfig-paths';
@@ -5,6 +6,11 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/config/tests.jsx',
+  },
   server: {
     port: 3000,
   },
