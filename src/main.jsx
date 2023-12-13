569027a967f562585f7d1e9e92595e52e4370018
Kamil Urbanek <86608622+LAJT22@users.noreply.github.com>
2023-12-13 20:07:40 +0100

    chore/added-custom-colors&typography (#2)
    * added custom colors&typography
    
    * chore/update theming task and transfer theme provider & global style from root to main
    
    * chore/updated proper hierarchy, weight added to typography

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { router } from '@routing/Router';
+import GlobalStyle from '@styles/globalStyles';
+import { GlobalThemeProvider } from '@theme/GlobalThemeProvider';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <GlobalThemeProvider>
+    <GlobalStyle />
+      <RouterProvider router={router} />
+    </GlobalThemeProvider>
   </React.StrictMode>
 );
