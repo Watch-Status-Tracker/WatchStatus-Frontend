7e7c13c9f4b84c6b92a423fb8224a4468350aecf
Kamil <kamilurbanek22@gmail.com>
2023-12-29 23:50:02 +0100

    ref/deleted router paths

--- src/routing/Router.jsx
+++ src/routing/Router.jsx
@@ -1,60 +1,9 @@
 import Root from '@/Root';
-import Playground from '@pages/Playground/Playground';
 import { Navigate, createBrowserRouter } from 'react-router-dom';
 
 const RootRoute = {
   path: '/',
   element: <Root />,
-  // children: [
-  //   {
-  //     path: '/home',
-  //   },
-  //   {
-  //     path: '/mylist',
-  //   },
-  //   {
-  //     path: '/browse',
-  //   },
-  //   {
-  //     path: '/surpriseme',
-  //   },
-  //   {
-  //     path: '/ranking',
-  //   },
-  //   {
-  //     path: '/settings',
-  //   },
-  //   {
-  //     path: '/logout',
-  //   },
-  // ],
-};
-
-const Home = {
-  path: '/home',
-};
-
-const MyList = {
-  path: '/mylist',
-};
-const Browse = {
-  path: '/browse',
-};
-const SurpriseMe = {
-  path: '/surpriseme',
-};
-const Ranking = {
-  path: '/ranking',
-};
-const Settings = {
-  path: '/settings',
-};
-const Logout = {
-  path: '/logout',
-};
-const PlaygroundRoute = {
-  path: '/playground',
-  element: <Playground />,
 };
 
 const WildcardRoute = {
@@ -62,18 +11,7 @@ const WildcardRoute = {
   element: <Navigate to="/" />,
 };
 
-export const router = createBrowserRouter([
-  Home,
-  MyList,
-  Browse,
-  SurpriseMe,
-  Ranking,
-  Settings,
-  Logout,
-  PlaygroundRoute,
-  RootRoute,
-  WildcardRoute,
-]);
+export const router = createBrowserRouter([RootRoute, WildcardRoute]);
 
 /*
   If you want to add new route,
