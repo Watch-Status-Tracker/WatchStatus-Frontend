17b13c01b39cd54dfb8caba047f5f1802c65b679
Numer <numerpriv@gmail.com>
2023-12-31 19:07:39 +0100

    ref: changed prop `size` to `variant` & added wrapper

--- src/components/Navbar/NavbarItem/NavbarItem.jsx
+++ src/components/Navbar/NavbarItem/NavbarItem.jsx
@@ -2,18 +2,21 @@ import {
   IconPlaceHolder,
   LinkText,
   StyledLink,
+  Wrapper,
 } from '@components/Navbar/NavbarItem/NavbarItem.styles';
 import PropTypes from 'prop-types';
 import { useLocation } from 'react-router-dom';
 
-const NavbarItem = ({ text, to, onClick, isTextVisible }) => {
+const NavbarItem = ({ text, to, onClick, variant }) => {
   const { pathname } = useLocation();
   const isActive = to === pathname;
   return (
-    <StyledLink isActive={isActive} to={to} onClick={onClick}>
-      <IconPlaceHolder isActive={isActive} />
-      {isTextVisible === 'full' && <LinkText isActive={isActive}>{text}</LinkText>}
-    </StyledLink>
+    <Wrapper isActive={isActive}>
+      <StyledLink isActive={isActive} to={to} onClick={onClick}>
+        <IconPlaceHolder isActive={isActive} />
+        {variant === 'full' && <LinkText isActive={isActive}>{text}</LinkText>}
+      </StyledLink>
+    </Wrapper>
   );
 };
 
@@ -21,7 +24,7 @@ NavbarItem.propTypes = {
   text: PropTypes.string,
   to: PropTypes.string,
   onClick: PropTypes.func,
-  isTextVisible: PropTypes.oneOf(['full', 'compact']),
+  variant: PropTypes.oneOf(['full', 'compact']),
 };
 
 export default NavbarItem;
