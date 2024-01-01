e5c8cbe8906cb530f5ef0643ac8d86b031720bae
Numer <numerpriv@gmail.com>
2024-01-02 00:26:08 +0100

    add: Icon component with variants

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,19 @@
+import { Bookmark } from '@components/Icon/IconComponents/Bookmark';
+import { Home } from '@components/Icon/IconComponents/Home';
+import { Logout } from '@components/Icon/IconComponents/Logout';
+import { Ranking } from '@components/Icon/IconComponents/Ranking';
+import { Search } from '@components/Icon/IconComponents/Search';
+import { Settings } from '@components/Icon/IconComponents/Settings';
+import { Shuffle } from '@components/Icon/IconComponents/Shuffle';
+
+const Icon = {
+  Home,
+  Search,
+  Bookmark,
+  Shuffle,
+  Ranking,
+  Settings,
+  Logout,
+};
+
+export default Icon;
