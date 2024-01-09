8d2ef629b70ec2bc92dc67c3f8f5dafe8498ba71
Numer <numerpriv@gmail.com>
2024-01-09 01:59:52 +0100

    add: custom form component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,43 @@
+import { useFormik } from 'formik';
+import { Children, cloneElement, memo, useMemo } from 'react';
+
+const Form = memo(({ children, initialValues, validationSchema, setErrors, onFormSubmit }) => {
+  const { handleChange, values, handleSubmit, validateForm } = useFormik({
+    initialValues,
+    validationSchema,
+    validateOnChange: false,
+    onSubmit: (values) => {
+      onFormSubmit(values);
+    },
+  });
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const errors = await validateForm(values);
+    if (Object.keys(errors).length > 0) {
+      setErrors && setErrors(Object.values(errors));
+    } else {
+      handleSubmit(e);
+      setErrors && setErrors([]);
+    }
+  };
+
+  // Function  for later that checks if children are of type certain component (Icon.Bookmark in this case)
+  // const checkType = Children.map(children, (child) => (child.type === Icon.Bookmark ? 'yes' : 'no'))
+
+  const mappedChildren = useMemo(
+    () =>
+      Children.map(children, (child) => {
+        const { name } = child.props;
+        return cloneElement(child, {
+          onChange: handleChange,
+          value: values[name],
+        });
+      }),
+    [children, handleChange, values]
+  );
+
+  return <form onSubmit={handleFormSubmit}>{mappedChildren}</form>;
+});
+
+export default Form;
