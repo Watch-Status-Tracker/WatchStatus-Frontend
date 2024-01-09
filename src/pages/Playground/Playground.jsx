6838bacb61fa0c6108d8208289408d1208de87f1
Numer <numerpriv@gmail.com>
2024-01-09 02:01:46 +0100

    add: temporary form with validation example

--- src/pages/Playground/Playground.jsx
+++ src/pages/Playground/Playground.jsx
@@ -1,9 +1,63 @@
+import Form from '@components/Form/Form';
+import { useState } from 'react';
 import styled from 'styled-components';
+import * as yup from 'yup';
+
+const predefinedValues = ['John', 'Doe', 'Jane'];
+
+const validationSchema = yup.object({
+  name: yup.string().oneOf(predefinedValues, 'Invalid value').required('Name is required'),
+  password: yup
+    .string()
+    .typeError('Password should be a string')
+    .required('Password is required')
+    .strict()
+    .min(6, 'Password must be at least 6 characters'),
+  email: yup.string().email('Invalid email').required('Email is required'),
+});
+
+const initialValues = {
+  name: '',
+  password: '',
+  email: '',
+};
+
+const handleSubmit = (values) => {
+  alert('It works! Look at console');
+  console.log(values);
+};
 
 const Playground = () => {
+  const [errorMessages, setErrorMessages] = useState([]);
   return (
     <Placeholder>
-      <p>Page for e.g. component testing</p>
+      <div>
+        <h2>Fields requirements</h2>
+        <p>Name: {predefinedValues.join(', ')}</p>
+        <p>Password: min 6 characters</p>
+        <p>Email: valid email | example: test@mail.pl </p>
+      </div>
+
+      <ul>
+        <h2>Errors</h2>
+        {errorMessages.map((error, index) => (
+          <li key={error + index}>{error}</li>
+        ))}
+      </ul>
+
+      <Form
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        setErrors={setErrorMessages}
+        onFormSubmit={handleSubmit}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <input type="text" name={'name'} />
+          <input type="password" name={'password'} />
+          <input type="text" name={'email'} />
+          <button type="submit">Submit</button>
+        </div>
+      </Form>
     </Placeholder>
   );
 };
@@ -13,6 +67,12 @@ export default Playground;
 const Placeholder = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
   align-items: center;
   height: 100vh;
+  gap: 24px;
+
+  li {
+    color: red;
+  }
 `;
