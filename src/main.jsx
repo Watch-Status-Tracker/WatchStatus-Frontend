a4765989189d1ead25a54ed4017944da4bff919a
Numer <numerpriv@gmail.com>
2024-01-11 01:03:34 +0100

    add: react query provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import GlobalStyle from '@styles/globalStyles';
 import { GlobalThemeProvider } from '@theme/GlobalThemeProvider';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouterProvider } from 'react-router-dom';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalThemeProvider>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-    </GlobalThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <GlobalThemeProvider>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </GlobalThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
