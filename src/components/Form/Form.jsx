8a99e99267c1892c7f53cbd96b87c0eb53125c64
Numer <numerpriv@gmail.com>
2024-01-17 02:30:47 +0100

    ref: exported submitForm from form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,8 +2,10 @@ import { useFormik } from 'formik';
 import { PropTypes } from 'prop-types';
 import { Children, cloneElement, isValidElement, memo } from 'react';
 
+export let submitFormm;
+
 const Form = memo(({ children, initialValues, validationSchema, setErrors, onFormSubmit }) => {
-  const { handleChange, values, handleSubmit, validateForm } = useFormik({
+  const { handleChange, values, handleSubmit, validateForm, submitForm } = useFormik({
     initialValues,
     validationSchema,
     validateOnChange: false,
@@ -11,6 +13,8 @@ const Form = memo(({ children, initialValues, validationSchema, setErrors, onFor
       onFormSubmit(values);
     },
   });
+
+  submitFormm = submitForm;
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
