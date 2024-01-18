5be64dc88ece705aed169207d01b8a20b704b40e
Numer <numerpriv@gmail.com>
2024-01-18 01:09:00 +0100

    add: backend endpoints for changing user data

--- src/config/api/backendAPI.js
+++ src/config/api/backendAPI.js
@@ -3,3 +3,9 @@ import { backendClient } from '@config/api/requests';
 export const register = (data) => backendClient.post(`/register`, data);
 
 export const login = (data) => backendClient.post(`/login`, data);
+
+export const changePersonalData = (data) => backendClient.post(`/change-personal-data`, data);
+
+export const changeAdditionalData = (data) => backendClient.post(`/change-additional-data`, data);
+
+export const changePasswordData = (data) => backendClient.post(`/change-password-data`, data);
