24bb203cf4ae6944749c7020ce00fd5f704854ab
Numer <numerpriv@gmail.com>
2024-01-18 04:33:48 +0100

    ref: backend requests

--- src/config/api/backendAPI.js
+++ src/config/api/backendAPI.js
@@ -3,3 +3,7 @@ import { backendClient } from '@config/api/requests';
 export const register = (data) => backendClient.post(`/register`, data);
 
 export const login = (data) => backendClient.post(`/login`, data);
+
+export const createList = (data) => backendClient.post(`/create-list`, data);
+
+export const getLists = () => backendClient.get(`/get-user-lists`);
