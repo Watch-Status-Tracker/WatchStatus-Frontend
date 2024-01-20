99835f1b7b2ea0c621a444938d5e4a0c0051482a
Numer <numerpriv@gmail.com>
2024-01-20 18:31:05 +0100

    add: user data to account box

--- src/components/AccountBox/AccountBox.jsx
+++ src/components/AccountBox/AccountBox.jsx
@@ -5,16 +5,31 @@ import {
   UserNameText,
   Wrapper,
 } from '@components/AccountBox/AccoutBox.styles';
+import { getUserPersonalData } from '@config/api/backendAPI';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useQuery } from 'react-query';
 
 const AccountBox = () => {
+  const [personalData, setPersonalData] = useState(null);
+
+  useQuery(['getCurrentUserPersonalData', personalData], () => getUserPersonalData(), {
+    onSuccess: ({ data }) => {
+      setPersonalData(data);
+      console.log(data);
+    },
+    onError: (error) => {
+      toast.error('Something went wrong', error);
+    },
+    refetchOnWindowFocus: false,
+  });
+
   return (
     <Wrapper>
-      <ImgContainer>
-        <img src="" alt="" />
-      </ImgContainer>
+      <ImgContainer></ImgContainer>
       <TextContainer>
-        <UserNameText>User name</UserNameText>
-        <UserInfo>Placeholder</UserInfo>
+        <UserNameText>{personalData?.username ? personalData?.username : ''}</UserNameText>
+        <UserInfo>{personalData?.title ? personalData?.title : ''}</UserInfo>
       </TextContainer>
     </Wrapper>
   );
