8c51909226cb868885a7c4f93bd0d5c4ee74bdef
Marta <martasmagowska@gmail.com>
2024-01-21 18:04:14 +0100

    my_lists tests

--- cypress/e2e/my_lists.cy.js
+++ cypress/e2e/my_lists.cy.js
@@ -2,7 +2,11 @@ import {
   loginPasswordInput,
   loginSubmit,
   loginUsernameInput,
+  myListInput,
+  myListsCreateListButton,
+  myListsWrapper,
   navbarListsButton,
+  toastSelector,
 } from '../fixtures/selectors';
 
 describe('My lists section display', () => {
@@ -15,9 +19,23 @@ describe('My lists section display', () => {
 
   it('Should check if My lists section displays correctly', () => {
     cy.get(navbarListsButton).contains('My lists').click();
+    cy.get(myListsWrapper).contains('My lists').and('be.visible');
   });
 
-  it('Should check if creating new list works correctly', () => {});
+  it('Should check if creating new list works correctly', () => {
+    cy.get(navbarListsButton).contains('My lists').click();
+    cy.get(myListInput).type('Test list');
+    cy.get(myListsCreateListButton).click();
+    cy.get(myListsWrapper).contains('Test list');
+  });
 
-  it('Should check if adding an intem to the list works correctly', () => {});
+  it('Should check if adding an item to the list works correctly', () => {
+    cy.get('[data-testid="card-container"]').first().trigger('mouseover');
+    cy.get(
+      ':nth-child(2) > .sc-jJEJze > .sc-cBNeRQ > :nth-child(1) > [data-testid="card-container"] > [data-testid="card-overlay"] > :nth-child(4)'
+    )
+      .first()
+      .click();
+    cy.get(toastSelector).contains('Position added to list!');
+  });
 });
